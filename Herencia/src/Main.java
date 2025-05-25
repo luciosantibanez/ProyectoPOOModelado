46c3404acc56215aa5afbb8a9df6b2147605b5ac
Lucio Santibañez <luciosantibanez99@gmail.com>
2025-05-25 20:57:19 -0300

    errores corregidos al 100 y agregamos atributos acorde a mi idea y mis clases

--- Herencia/src/Main.java
+++ Herencia/src/Main.java
@@ -5,7 +5,15 @@ public class Main {
 
         Mochila mochila = new Mochila();
 
+        Juguete muñeco = new Juguete("Payaso", "verde", "Coleccionable");
+        Ropa gorra = new Ropa("accesorio");
+        Ropa gorro = new Ropa("gorro", "negro","accesorio");
+        Item itemGenerico = new Item("Notebook","gris");
 
+        mochila.addItem(muñeco);
+        mochila.addItem(gorra);
+        mochila.addItem(gorro);
+        mochila.addItem(itemGenerico);
 
         mochila.displayMochila();
 
